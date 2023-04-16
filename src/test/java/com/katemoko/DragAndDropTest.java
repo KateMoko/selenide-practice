a60ecebc7dd26bc50ee5cdc36ead688b1ce29ac9
Kate Moiseenko <katfedoseeva@gmail.com>
2023-04-16 16:54:53 +0300

    draganddrop

--- src/test/java/com/katemoko/DragAndDropTest.java
+++ src/test/java/com/katemoko/DragAndDropTest.java
@@ -1,2 +1,23 @@
-package com.katemoko;public class DragAndDropTest {
+package com.katemoko;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest extends TestBase {
+    SelenideElement columnA = $("#column-a"),
+            columnB = $("#column-b");
+
+    @Test
+    void moveARectangleToBPlace() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        columnA.shouldHave(text("A"));
+        columnB.shouldHave(text("B"));
+        columnA.dragAndDropTo(columnB);
+        sleep(5000);
+        columnA.shouldHave(text("B"));
+        columnB.shouldHave(text("A"));
+    }
 }
